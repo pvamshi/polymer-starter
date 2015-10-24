435535b34c138152d3b9443913161eb324e5793f
Vamshi Krishna Pothuka <pvamshi@gmail.com>
2015-10-24 11:30:16 -0700

    Drag to draw circle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -67,8 +67,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   // Scroll page to top and expand header
-  app.scrollPageToTop = function() {
-    document.getElementById('mainContainer').scrollTop = 0;
-  };
+  // app.scrollPageToTop = function() {
+  //   document.getElementById('mainContainer').scrollTop = 0;
+  // };
 
 })(document);
